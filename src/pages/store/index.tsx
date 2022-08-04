251eb19569ab0aa2c79cc54c82320186e2b58605
Edilson Almeida <edifilho1409@gmail.com>
2022-08-04 10:45:36 -0300

    new: Store Item
    Store Item style finished

--- src/pages/store/index.tsx
+++ src/pages/store/index.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import './index.css';
+import storeItems from '../../data/items.json';
+import { Col, Row } from 'react-bootstrap';
+import StoreItem from '../../components/StoreItem';
 
 const Store = () => {
     return (
-        <div>E aí, Store!</div>
+        <>
+            <h1>Store</h1>
+            <Row md={2} xs={1} lg={3} className="g-3">
+                {storeItems.map((item: any, id: number) => (
+                    <Col key={id}>
+                        <StoreItem {...item} />
+                    </Col>
+                ))}
+            </Row>
+        </>
     )
 }
 
