16636287b2d0b467f06d1054982df80fe417094e
Edilson Almeida <edifilho1409@gmail.com>
2022-08-23 10:56:06 -0300

    new: Shopping Cart Finished
    Now, we have a pretty shopping cart, it's new, it's ALIVE

--- src/components/ShoppingCart/shopping-cart.tsx
+++ src/components/ShoppingCart/shopping-cart.tsx
@@ -1,18 +1,39 @@
 import React from 'react';
-import { Offcanvas } from 'react-bootstrap';
+import { Offcanvas, OffcanvasBody, Stack } from 'react-bootstrap';
 import { useShoppingCart } from '../../context/ShoppingCartContext';
+import { FormatCurrency } from '../../utilities/formatCurrency';
+import CartItem from '../CartItem';
+import storeItem from '../../data/items.json';
 
 type ShoppingCartProps = {
     isOpen: boolean
 }
 
 const ShoppingCart = ({ isOpen }: ShoppingCartProps) => {
-    const { openCart } = useShoppingCart();
+    const { openCart, cartItems } = useShoppingCart();
     return (
         <Offcanvas show={isOpen} onHide={openCart} placement="end">
-            <Offcanvas.Header>
+            <Offcanvas.Header closeButton>
                 <Offcanvas.Title>Cart</Offcanvas.Title>
             </Offcanvas.Header>
+            <OffcanvasBody>
+                <Stack gap={3}>
+                    {cartItems.map(item => {
+                        return (
+                            <CartItem key={item.id} {...item} />
+                        )
+                    })}
+                    <div className="ms-auto fw-bold fs-5">
+                        Total{" "}
+                        {FormatCurrency(
+                            cartItems.reduce((total, cartItem) => {
+                                const item = storeItem.find(i => i.id === cartItem.id)
+                                return total + (item?.price || 0) * cartItem.quantity
+                            }, 0)
+                        )}
+                    </div>
+                </Stack>
+            </OffcanvasBody>
         </Offcanvas>
     )
 }
